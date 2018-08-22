8661706283fac11380bcb3909519d7144f8cb674
SashaDeviaten <sasha_deviaten@mail.ru>
2018-08-22 23:41:04 +0300

    remove react-fancybox

--- Pages/Page_Clients/Page_Clients.js
+++ Pages/Page_Clients/Page_Clients.js
@@ -1,45 +1,25 @@
-﻿// import React from 'react';
-// import './Page_Clients.scss'
-//
-//
-// class Page_Clients extends React.PureComponent {
-//
-//     mainClassName = 'Page_Clients';
-//
-//   render() {
-//       const {mainClassName} = this;
-//
-//     return (
-//
-//
-//         <div className={mainClassName}>
-//
-//         </div>
-//
-//     );
-//
-//   }
-//
-// }
-//
-// export default Page_Clients;
+﻿import React from 'react';
+import './Page_Clients.scss'
 
 
-import React, {Component} from 'react';
-import ReactFancyBox from 'react-fancybox'
-import 'react-fancybox/lib/fancybox.css'
+class Page_Clients extends React.PureComponent {
 
-class App1 extends Component {
-    render() {
-        return (
-            <div className="App">
+    mainClassName = 'Page_Clients';
 
-                <ReactFancyBox
-                    thumbnail="https://loremflickr.com/320/240"
-                    image="https://www.w3schools.com/howto/img_forest.jpg"/>
-            </div>
-        );
-    }
+  render() {
+      const {mainClassName} = this;
+
+    return (
+
+
+        <div className={mainClassName}>
+
+        </div>
+
+    );
+
+  }
+
 }
 
-export default App1;
+export default Page_Clients;
